9f6056c6e30ac2a0a4af07aa0d56963a492d8174
HONYIN KOK <honyin@gmail.com>
2021-09-13 08:20:52 -0400

    Added accounts

---
+++ acct_value/acct_value.py
@@ -0,0 +1,141 @@
+import logging
+
+import time
+
+
+import pandas as pd
+
+from ibapi.utils import iswrapper
+
+from ContractSamples import ContractSamples
+
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+# types
+from ibapi.common import *  # @UnusedWildImport
+from ibapi.contract import * # @UnusedWildImport
+
+class TestApp(EWrapper, EClient):
+    def __init__(self):
+        EWrapper.__init__(self)
+        EClient.__init__(self, wrapper=self)
+        # ! [socket_init]
+        self.nKeybInt = 0
+        self.started = False
+        self.nextValidOrderId = None
+        self.permId2ord = {}
+        self.globalCancelOnly = False
+        self.simplePlaceOid = None
+        self._my_errors = {}
+        self.data = []  # Initialize variable to store candle
+        self.df = pd.DataFrame()
+
+    @iswrapper
+    # ! [connectack]
+    def connectAck(self):
+        if self.asynchronous:
+            self.startApi()
+
+    # ! [connectack]
+
+    @iswrapper
+    # ! [nextvalidid]
+    def nextValidId(self, orderId: int):
+        super().nextValidId(orderId)
+
+        logging.debug("setting nextValidOrderId: %d", orderId)
+        self.nextValidOrderId = orderId
+        print("NextValidId:", orderId)
+        # ! [nextvalidid]
+
+        # we can start now
+        self.start()
+
+    def start(self):
+        if self.started:
+            return
+
+        self.started = True
+
+        if self.globalCancelOnly:
+            print("Executing GlobalCancel only")
+            self.reqGlobalCancel()
+        else:
+            print("Executing requests")
+            self.accountOperations_req()
+            print("Executing requests ... finished")
+
+    def keyboardInterrupt(self):
+        self.nKeybInt += 1
+        if self.nKeybInt == 1:
+            self.stop()
+        else:
+            print("Finishing test")
+            self.done = True
+
+    def stop(self):
+        print("Executing cancels")
+
+
+        print("Executing cancels ... finished")
+
+    def nextOrderId(self):
+        oid = self.nextValidOrderId
+        self.nextValidOrderId += 1
+        return oid
+
+    @iswrapper
+    # ! [error]
+    def error(self, reqId: TickerId, errorCode: int, errorString: str):
+        super().error(reqId, errorCode, errorString)
+        print("Error. Id:", reqId, "Code:", errorCode, "Msg:", errorString)
+        errormsg = "IB error id %d errorcode %d string %s" % (reqId, errorCode, errorString)
+        self._my_errors = errormsg
+
+    @iswrapper
+    def winError(self, text: str, lastError: int):
+        super().winError(text, lastError)
+
+    def accountOperations_req(self):
+        # Requesting accounts' summary
+        # ! [reqaaccountsummary]
+        self.reqAccountSummary(9002, "All", "$LEDGER")
+        # ! [reqaaccountsummary]
+
+    # ! [accountsummary]
+    def accountSummary(self, reqId: int, account: str, tag: str, value: str,
+                       currency: str):
+        super().accountSummary(reqId, account, tag, value, currency)
+        print("AccountSummary. ReqId:", reqId, "Account:", account,
+              "Tag: ", tag, "Value:", value, "Currency:", currency)
+        self.data.append([tag, value])
+        self.df = pd.DataFrame(self.data, columns=['Account', 'Value'])
+        print(self.df)
+        self.df.to_csv('acct_value.csv')
+        if len(self.df) == 24:
+            net_liquid = self.df.loc[8,'Value']
+            five_pct_rule = .05 * float(net_liquid)
+            print(f'net liquidation value: {net_liquid}')
+            print(f'5% allocation:  {five_pct_rule}')
+        self.disconnect()
+
+def main():
+
+    app = TestApp()
+    try:
+
+        # ! [connect]
+        app.connect("127.0.0.1", port=7497, clientId=102)
+        # ! [connect]
+        print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),
+                                                      app.twsConnectionTime()))
+        # ! [clientrun]
+        app.run()
+
+
+        # ! [clientrun]
+    except:
+        raise
+
+if __name__ == "__main__":
+    main()
---
+++ acct_value/open_orders.py
@@ -0,0 +1,166 @@
+import logging
+
+import time
+
+
+import pandas as pd
+
+from ibapi.utils import iswrapper
+
+from ContractSamples import ContractSamples
+
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+# types
+from ibapi.common import *  # @UnusedWildImport
+from ibapi.contract import * # @UnusedWildImport
+from ibapi.order import Order
+from ibapi.order_state import OrderState
+
+class TestApp(EWrapper, EClient):
+    def __init__(self):
+        EWrapper.__init__(self)
+        EClient.__init__(self, wrapper=self)
+        # ! [socket_init]
+        self.nKeybInt = 0
+        self.started = False
+        self.nextValidOrderId = None
+        self.permId2ord = {}
+        self.globalCancelOnly = False
+        self.simplePlaceOid = None
+        self._my_errors = {}
+        self.data = []  # Initialize variable to store candle
+        self.df = pd.DataFrame()
+
+    @iswrapper
+    # ! [connectack]
+    def connectAck(self):
+        if self.asynchronous:
+            self.startApi()
+
+    # ! [connectack]
+
+    @iswrapper
+    # ! [nextvalidid]
+    def nextValidId(self, orderId: int):
+        super().nextValidId(orderId)
+
+        logging.debug("setting nextValidOrderId: %d", orderId)
+        self.nextValidOrderId = orderId
+        print("NextValidId:", orderId)
+        # ! [nextvalidid]
+
+        # we can start now
+        self.start()
+
+    def start(self):
+        if self.started:
+            return
+
+        self.started = True
+
+        if self.globalCancelOnly:
+            print("Executing GlobalCancel only")
+            self.reqGlobalCancel()
+        else:
+            print("Executing requests")
+            self.orderOperations_req()
+
+            print("Executing requests ... finished")
+
+    def keyboardInterrupt(self):
+        self.nKeybInt += 1
+        if self.nKeybInt == 1:
+            self.stop()
+        else:
+            print("Finishing test")
+            self.done = True
+
+    def stop(self):
+        print("Executing cancels")
+
+
+        print("Executing cancels ... finished")
+
+    def nextOrderId(self):
+        oid = self.nextValidOrderId
+        self.nextValidOrderId += 1
+        return oid
+
+    @iswrapper
+    # ! [error]
+    def error(self, reqId: TickerId, errorCode: int, errorString: str):
+        super().error(reqId, errorCode, errorString)
+        print("Error. Id:", reqId, "Code:", errorCode, "Msg:", errorString)
+        errormsg = "IB error id %d errorcode %d string %s" % (reqId, errorCode, errorString)
+        self._my_errors = errormsg
+
+    @iswrapper
+    def winError(self, text: str, lastError: int):
+        super().winError(text, lastError)
+
+    def orderOperations_req(self):
+        # Requesting all open orders
+        # ! [reqallopenorders]
+        self.reqAllOpenOrders()
+        # ! [reqallopenorders]
+
+    def openOrder(self, orderId: OrderId, contract: Contract, order: Order,
+                  orderState: OrderState):
+        super().openOrder(orderId, contract, order, orderState)
+        print("OpenOrder. PermId: ", order.permId, "ClientId:", order.clientId, " OrderId:", orderId,
+              "Account:", order.account, "Symbol:", contract.symbol, "SecType:", contract.secType,
+              "Exchange:", contract.exchange, "Action:", order.action, "OrderType:", order.orderType,
+              "TotalQty:", order.totalQuantity, "CashQty:", order.cashQty,
+              "LmtPrice:", order.lmtPrice, "AuxPrice:", order.auxPrice, "Status:", orderState.status)
+
+        order.contract = contract
+        self.permId2ord[order.permId] = order
+        # ! [openorder]
+
+    # ! [openorderend]
+    def openOrderEnd(self):
+        super().openOrderEnd()
+        print("OpenOrderEnd")
+
+        logging.debug("Received %d openOrders", len(self.permId2ord))
+
+    # ! [openorderend]
+
+
+    # ! [orderstatus]
+    def orderStatus(self, orderId: OrderId, status: str, filled: float,
+                    remaining: float, avgFillPrice: float, permId: int,
+                    parentId: int, lastFillPrice: float, clientId: int,
+                    whyHeld: str, mktCapPrice: float):
+        super().orderStatus(orderId, status, filled, remaining,
+                            avgFillPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice)
+        print("OrderStatus. Id:", orderId, "Status:", status, "Filled:", filled,
+              "Remaining:", remaining, "AvgFillPrice:", avgFillPrice,
+              "PermId:", permId, "ParentId:", parentId, "LastFillPrice:",
+              lastFillPrice, "ClientId:", clientId, "WhyHeld:",
+              whyHeld, "MktCapPrice:", mktCapPrice)
+        self.disconnect()
+    # ! [orderstatus]
+
+
+def main():
+
+    app = TestApp()
+    try:
+
+        # ! [connect]
+        app.connect("127.0.0.1", port=7497, clientId=102)
+        # ! [connect]
+        print("serverVersion:%s connectionTime:%s" % (app.serverVersion(),
+                                                      app.twsConnectionTime()))
+        # ! [clientrun]
+        app.run()
+
+
+        # ! [clientrun]
+    except:
+        raise
+
+if __name__ == "__main__":
+    main()
